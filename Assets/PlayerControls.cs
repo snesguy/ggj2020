8ac79d20318219761646baa1af4d2e7be5ec4181
Vuong Le <vuongle6@gmail.com>
2020-02-01 08:57:47 -0600

    initial commit

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour, ItemControl
+{
+    [SerializeField]
+    GameObject player;
+
+    public void controlPart()
+    {
+        Input.GetKey(KeyCode.UpArrow){
+            player.transform.position.
+        }
+
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    ItemControl playerControls;
+
+    public enum PlayerStates
+    {
+        PLAYER = 0, CONTROL0 = 1
+    }
+
+    public PlayerStates PlayerState { get; set; } = PlayerStates.PLAYER;
+
+    void Start()
+    {
+        switch (PlayerState)
+        {
+            case PlayerStates.PLAYER:
+
+                break;
+            case PlayerStates.CONTROL0:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
