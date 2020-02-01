3c80883bbb2d37c23eee57a4ee176eaa9135d30c
James <kingdon909@gmail.com>
2020-02-01 16:00:33 -0500

    destructable ground

---
+++ Assets/James/CreateSquareGrid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSquareGrid : MonoBehaviour
+{
+    public GameObject square;
+    public int width = 0;
+    public int height = 0;
+    public float spacing = .15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGrid(width, height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateGrid(int width, int height)
+    {
+        Vector3 location = new Vector3(0, 0, 0);
+        for (location.y = 0; location.y < height; location.y += spacing)
+        {
+            for (location.x = 0; location.x < width; location.x += spacing)
+            {
+                GameObject.Instantiate(square, location, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/James/Square.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bullet bullet;
+        if(null != (bullet = collision.gameObject.GetComponent<Bullet>()))
+        {
+            TakeDamage(bullet.damage);
+        }
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+        if(health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
