4684dcf59d1bb875896c19fc967e70ae451929e2
J3ThePainless <aaronjratcliffe@gmail.com>
2020-02-02 14:42:29 -0500

    added gear pile

---
+++ Assets/Scripts/Managers/PlayerInventory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public bool handsFull;
+    public int uranium;
+    public int gear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handsFull = false;
+        gear = 0;
+        uranium = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(uranium > 0 || gear > 0)
+        {
+            handsFull = true;
+        }
+        else
+        {
+            handsFull = false;
+        }
+    }
+    public int getGear()
+    {
+        return gear;
+    }
+    public void addGear(int newGear)
+    {
+
+        gear += newGear;
+    }
+    public void useGear(int gearUsed)
+    {
+
+        gear -= gearUsed;
+    }
+    public int getUranium()
+    {
+        return uranium;
+    }
+    public void addUranium(int newUranium)
+    {
+
+        uranium += newUranium;
+    }
+    public void useUranium(int uraniumUsed)
+    {
+
+        uranium += uraniumUsed;
+    }
+}
