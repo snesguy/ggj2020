1dc6231f56a08e5cc868741a46f143981c964373
Bobjrsenior <bobjrsenior@bobjrsenior.pw>
2020-02-02 13:41:54 -0500

    Add jump mechanic

---
+++ Assets/Scripts/ItemControls/JumpControls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpControls : ItemControl
+{
+
+    public float baseVelocity = 2.0f;
+
+    public float steamVelocityMultiplier = 1.1f;
+    private Rigidbody2D body;
+
+    public bool jump;
+
+    public float devPresureAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if(jump)
+        {
+            jump = false;
+            Jump(devPresureAmount);
+        }
+
+        ControlPart();
+    }
+
+    public override void ControlPart()
+    {
+
+    }
+
+    public void Jump(float pressure)
+    {
+        body.velocity = transform.up * (baseVelocity + (pressure * steamVelocityMultiplier));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
